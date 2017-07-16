4be5b7a919a5983211dd3386a96da987e1fa6364
jamesrhee91 <jamesshrhee@gmail.com>
2017-07-16 16:55:35 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,17 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+
+    arr = [0, 0, 0]
+    arr.map! {|n| Kernel.rand(1..20)}
+    arr.each {|n| resp.write "#{n}\n"}
+
+    if arr[0] == arr[1] && arr[1] == arr[2]
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
+
     resp.finish
   end
 
